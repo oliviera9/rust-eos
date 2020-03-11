3775839d819da0ba1821ec024eb62e294cdb147e
Greg Kapka <greg@oraclize.it>
2020-03-11 11:28:39 +0000

    feat(extension): <- adds initter for that

--- primitives/src/extension.rs
+++ primitives/src/extension.rs
@@ -5,7 +5,17 @@ use serde::{Deserialize, Serialize};
 #[derive(Read, Write, NumBytes, PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Hash, Default)]
 #[cfg_attr(feature = "std", derive(Deserialize, Serialize))]
 #[eosio_core_root_path = "crate"]
-pub struct Extension(u16, Vec<u8>);
+pub struct Extension(pub u16, pub Vec<u8>);
+
+impl Extension {
+    pub fn new(bytes: Vec<u8>) -> Self {
+        let mut array = [0; 2];
+        let u16_bytes = &bytes[..array.len()];
+        array.copy_from_slice(u16_bytes);
+        let u_16 = u16::from_le_bytes(array);
+        Extension(u_16, bytes[2..].to_vec())
+    }
+}
 
 impl core::fmt::Display for Extension {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
